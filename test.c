9c6a51cd66d9449113176e299d3599a2027a8033
Adolfo G <141743766+starzlocker@users.noreply.github.com>
2025-01-06 14:10:07 -0300

    Update test.c

--- test.c
+++ test.c
@@ -8,7 +8,7 @@ int main()
         struct node *next;
     }typedef node;
 
-    //declaring nodes dasdas
+    //declaring nodes dasdasddddddddddssss
     struct node *a,*b,*c;
 
     //Implement task 1 here
